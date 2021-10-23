24c95e5ae67031762a38f492a2dbd73bfe9245c7
mitali2608 <mitalibawankule21@gmail.com>
2021-10-23 10:20:25 +0530

    Program class added

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tic Tac Toe game :");
+            TicTacToeGame t = new TicTacToeGame();
+            t.Board();
+        }
+    }
+}
