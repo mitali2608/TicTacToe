0192a2552594694a5f8f46450e6e9fdd2e0332e8
mitali2608 <mitalibawankule21@gmail.com>
2021-10-23 10:19:56 +0530

    TicTacToe Class added

---
+++ TicTacToeGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class TicTacToeGame
+    {
+        char[] a = new char[ 10 ];
+        public void Board()
+        {
+            for (int i = 1; i < a.Length; i++)
+                a[i] = ' ';
+            {
+                int i = 0;
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
