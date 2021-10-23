143760964f6677145536e813d47138ea5a6af8ef
mitali2608 <mitalibawankule21@gmail.com>
2021-10-23 10:55:07 +0530

    UC2 added to select X or 0

---
+++ SelectXor0.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class SelectXor0
+    {
+        public void select()
+        {
+            Console.WriteLine(" Player 1 - Select option 'X' OR 'O' ");
+            string S1 = Console.ReadLine();
+
+
+            if (S1 == "X")
+            {
+                Console.WriteLine("Player 1 selected 'X'");
+                Console.WriteLine("Player 2 selected 'O'");
+            }
+            else
+            {
+                Console.WriteLine("Player 1 selected 'O'");
+                Console.WriteLine("Player 2 selected 'X'");
+            }
+        }
+    }
+}
