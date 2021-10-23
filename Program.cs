0f96a123081bbb21d524f50272defa51f983884d
Mitali Bawankule <88092878+mitali2608@users.noreply.github.com>
2021-10-23 10:53:07 +0530

    Main program added

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace TicTacToe
             Console.WriteLine("Tic Tac Toe game :");
             TicTacToeGame t = new TicTacToeGame();
             t.Board();
+            
+            SelectXor0 uc2 = new SelectXor0();
+            uc2.select();
         }
     }
 }
